01861a35f0d22f396f38806d321cfe01844c5a0d
Antonio Ricardo <antonioricardoarfs@gmail.com>
2021-03-09 21:23:07 -0400

    create livewire config

--- resources/views/panels/styles.blade.php
+++ resources/views/panels/styles.blade.php
@@ -1,18 +1,18 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{ mix('vendors/vendors.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('vendors/vendors.min.css') }}">
 <!-- BEGIN: VENDOR CSS-->
 @yield('vendor-style')
 <!-- END: VENDOR CSS-->
 <!-- BEGIN: Page Level CSS-->
 @if(!empty($configData['mainLayoutType']) && isset($configData['mainLayoutType']))
 <link rel="stylesheet" type="text/css"
-  href="{{mix('css/themes/'.$configData['mainLayoutType'].'-template/materialize.css')}}">
+  href="{{asset('css/themes/'.$configData['mainLayoutType'].'-template/materialize.css')}}">
 <link rel="stylesheet" type="text/css"
-  href="{{mix('css/themes/'.$configData['mainLayoutType'].'-template/style.css')}}">
+  href="{{asset('css/themes/'.$configData['mainLayoutType'].'-template/style.css')}}">
 
 @if($configData['mainLayoutType'] === 'horizontal-menu')
 {{-- horizontal style file only for horizontal layout --}}
-<link rel="stylesheet" type="text/css" href="{{mix('css/layouts/style-horizontal.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/layouts/style-horizontal.css')}}">
 @endif
 @else
 <h1>mainLayoutType option is either empty or not set in config custom.php file.</h1>
