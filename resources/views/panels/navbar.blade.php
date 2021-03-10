300816336d0e1b7a7ed4633f862248bedfeafc98
Antonio Ricardo <antonioricardoarfs@gmail.com>
2021-03-09 20:46:43 -0400

    adjust package json

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -14,7 +14,7 @@
           <a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);"
             data-target="profile-dropdown">
             <span class="avatar-status avatar-online">
-              <img src="{{asset('images/avatar/avatar-7.png')}}" alt="avatar"><i></i>
+              <img src="/images/avatar/avatar-7.png" alt="avatar"><i></i>
             </span>
           </a>
         </li>
